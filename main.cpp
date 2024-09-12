3c6a5f523a323d2f28bd152aad232152d5aec39e
unielkarpavicius <unielkarpavicius@fei.edu.br>
2024-09-12 00:48:48 +0000

    adição da classe pessoa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
+#include "Pessoa.cpp"
 using namespace std;
 
-
 int main () {
-    cout << "Hello world\n";
+    Pessoa p;
+    p.setNome("Nome");
+    p.setSobrenome("Sobrenome");
+    p.setCPF(7865270256);
+
+    cout << "Dados da Pessoa" << endl;
+    cout << "Nome: " << p.getNome() << " " << p.getSobrenome() << endl;
+    cout << "CPF: " << p.getCPF() << endl;
+
 }
