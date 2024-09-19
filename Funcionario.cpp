172f697ac6014bd23642d95f11cae27022007f39
unielkarpavicius <unielkarpavicius@fei.edu.br>
2024-09-19 00:40:08 +0000

    ihfhgfkljfd

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 
 class Funcionario: public Pessoa{
-    
-    public:
-        Funcionario(string nome, string sobrenome, long cpf, int dia, int mes, int ano, Endereco endereco):  
-        Pessoa(nome, sobrenome, cpf, dia, mes, ano, endereco){}
 
-        
+    private:
+        double salario;
+        int cadastro;
+
+    public:
+        Funcionario(string nome, string sobrenome, long cpf, 
+            int dia, int mes, int ano, Endereco endereco, 
+            double salario, int cadastro):  
+        Pessoa(nome, sobrenome, cpf, dia, mes, ano, endereco),
+        salario{salario}, cadastro{cadastro}{}
+
+
 };
--- main.cpp
+++ main.cpp
@@ -7,16 +7,24 @@ using namespace std;
 
 int main () {
     Endereco e("Rua R", 1, "Bairro B");
-    Pessoa p ("Nome", "Sobrenome", 7865270256, 11, 9, 2001, e);
+    Pessoa p ("Pessoa", "Sobrenome", 7865270256, 11, 9, 2001, e);
     //p.setNome("Nome");
     //p.setSobrenome("Sobrenome");
     //p.setCPF(7865270256);
+    Funcionario f ("Funcionário", "Sobrenome", 7865270256, 11, 9, 2001, e, 1000, 1234);
 
     cout << "Dados da Pessoa" << endl;
     cout << "Nome: " << p.getNome() << " " << p.getSobrenome() << endl;
     cout << "CPF: " << p.getCPF() << endl;
     cout << "Data de Nascimento: " << p.getNascimento() << endl;
     cout << "Endereço: " << p.getEndereco() << endl;
+
+    cout << "Dados da Funcionário" << endl;
+    cout << "Nome: " << f.getNome() << " " << f.getSobrenome() << endl;
+    cout << "CPF: " << f.getCPF() << endl;
+    cout << "Data de Nascimento: " << f.getNascimento() << endl;
+    cout << "Endereço: " << f.getEndereco() << endl;
+    
 
     //int dia, mes, ano;
 
